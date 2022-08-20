b357497dedb17e76489a46a0b6eb9268b69fb5cb
David Wu <0830david@gmail.com>
2022-08-21 01:08:39 +0800

    Fixed Bugs

--- src/pages/auth/preface.js
+++ src/pages/auth/preface.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './auth.css';
+import Navbar from '../../navbar';
 // import jwtDecode from 'jwt-decode'
 // import { useEffect, useState } from 'react'
 // import { useNavigate } from "react-router-dom";
 
 function PrefacePage() {
 	return (
-		<div className='PrefacePage'>
-			<h1>前言</h1>
+		<div className='AuthPage'>
+			<div className='decoration-box'>
+                <Navbar/> 
+                <h1>前言</h1>
+            </div>
 			<footer>
 				Author: Magnus & David
 			</footer>
