b71dc5286d8d736b58707c0cdea6d4eab5b87387
David Wu <0830david@gmail.com>
2022-08-25 21:00:04 +0800

    Preface Fix

--- src/pages/others/preface.js
+++ src/pages/others/preface.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './preface.css';
-// import {Link} from 'react-scroll';
+import { useRef } from 'react'; 
 
 function PrefacePage () {
-	
+
+	const page2 = useRef(null);
+	const page3 = useRef(null);
+	const page4 = useRef(null);
+
+	const handleClick = (elementRef) => {
+		elementRef.current?.scrollIntoView({behavior:"smooth"})
+	};
 	
 	return (
 		<div className="PrefacePage">
 			<div className="preface">
-				<div id="0" className='pages'>
+				<div className='pages'>
 					<div className='big-box'>
 						<img src={require('../../images/preface/1.png')} alt="1"/>
 						<div className="box"/>
@@ -18,41 +25,64 @@ function PrefacePage () {
 							起油鍋，將雞肉下鍋炒至約7分熟後起鍋備用。
 							利用同一油鍋，將蔥白、薑、蒜下鍋爆香。
 							接著將乾辣椒與雞肉下鍋炒熟。
-							最後起鍋前再加入蔥綠、麻辣花生和適量的鹽與糖調味即可。</p>
+							最後起鍋前再加入蔥綠、麻辣花生和適量的鹽與糖調味即可。
+						</p>
 					</div>
-					<button>下一頁</button>
+					<button onClick={() => handleClick(page2)}>下一頁</button>
 				</div>
-				<div id="1" className='pages'>
+				<div ref={page2} className='pages'>
 					<div className='big-box'>
 						<img src={require('../../images/preface/2.png')} alt="2"/>
 						<div className="box"/>
 						<h1>前言二</h1>
 						<hr/>
-						<p></p>	
+						<p>
+						首先將雞肉切塊後與醃料均勻混合醃製15~20分鐘。
+							起油鍋，將雞肉下鍋炒至約7分熟後起鍋備用。
+							利用同一油鍋，將蔥白、薑、蒜下鍋爆香。
+							接著將乾辣椒與雞肉下鍋炒熟。
+							最後起鍋前再加入蔥綠、麻辣花生和適量的鹽與糖調味即可。</p>	
 					</div>
-					<button>下一頁</button>
+					<button onClick={() => handleClick(page3)}>下一頁</button>
 				</div>
-				<div id="2" className='pages'>
+				<div ref={page3} className='pages'>
 					<div className='big-box'>
 						<img src={require('../../images/preface/3.png')} alt="3"/>
 						<div className="box"/>
 						<h1>前言三</h1>
 						<hr/>
+						<p>
+						首先將雞肉切塊後與醃料均勻混合醃製15~20分鐘。
+							起油鍋，將雞肉下鍋炒至約7分熟後起鍋備用。
+							利用同一油鍋，將蔥白、薑、蒜下鍋爆香。
+							接著將乾辣椒與雞肉下鍋炒熟。
+							最後起鍋前再加入蔥綠、麻辣花生和適量的鹽與糖調味即可。
+						</p>
 					</div>
-					<button>下一頁</button>
+					<button onClick={() => handleClick(page4)}>下一頁</button>
 				</div>
-				<div id="3" className='pages'>
+				<div ref={page4} className='pages'>
 					<div className='big-box'>
 						<img src={require('../../images/preface/4.png')} alt="4"/>
 						<div className="box"/>
 						<h1>前言四</h1>
 						<hr/>
+						<p>
+						首先將雞肉切塊後與醃料均勻混合醃製15~20分鐘。
+							起油鍋，將雞肉下鍋炒至約7分熟後起鍋備用。
+							利用同一油鍋，將蔥白、薑、蒜下鍋爆香。
+							接著將乾辣椒與雞肉下鍋炒熟。
+							最後起鍋前再加入蔥綠、麻辣花生和適量的鹽與糖調味即可。
+						</p>
 					</div>
-					<button onClick={() => {window.location.href = '/all'}}>立即開始</button>
+					<div className='inline'>
+						<button className="inline-btn" onClick={() => {window.location.href = '/all'}}>立即開始</button>
+						<button className="inline-btn" onClick={() => {window.location.href = '/login'}}>登入</button>
+					</div>
 				</div>
 			</div>
 		</div>
-	);
-  };
+	);		
+};
 
 export default PrefacePage;
