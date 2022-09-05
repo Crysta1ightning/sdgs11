ac1bd2a9fe98c7aeaf01657da99332332b4615f6
David Wu <0830david@gmail.com>
2022-09-05 23:08:22 +0800

    Path Page & Spot Page Map Function

--- src/pages/path/path.js
+++ src/pages/path/path.js
@@ -1,6 +1,7 @@
 import './path.css';
 import Load from '../global/load';
 import { useEffect, useState, useCallback } from 'react'
+import { useJsApiLoader } from '@react-google-maps/api';
 import { useParams } from "react-router-dom";
 import BackKey from '../global/backkey';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -10,22 +11,43 @@ function PathPage() {
 	const {pathID} = useParams();
 	const [loading, setLoading] = useState(true);
 	const [valid, setValid] = useState(true);
-	const [userX, setUserX] = useState(0);
-    const [userY, setUserY] = useState(0);
+	const { isLoaded } = useJsApiLoader({
+		id: 'google-map-script',
+		googleMapsApiKey: "AIzaSyAWGySAUlZ2lcu2S9zt5B852RD6ghn3th8",
+	})
+	const [userLat, setUserLat] = useState(0);
+    const [userLng, setUserLng] = useState(0);
 	const [predictTime, setPredictTime] = useState(0);
 	const [spotList, setSpotList] = useState([
-		// {spotID: 1, x: 50, y: 150, name: "人社院", description: "人社院是迷宮", distance: 0},
-		// {spotID: 2, x: 70, y: 60, name: "台達館", description: "資工電神所在地", distance: 0},
-		// {spotID: 3, x: 80, y: 90, name: "小吃部", description: "小吃部只有麥當勞", distance: 0}, 
-		// {spotID: 4, x: 10, y: 10, name: "成功湖", description: "很大的一個湖", distance: 0},
-		// {spotID: 5, x: 20, y: 130, name: "教育館", description: "上通識課的地方", distance: 0},  
-        // {spotID: 6, x: 100, y: 100, name: "旺宏館", description: "這是旺宏館", distance: 0}, 
-        // {spotID: 7, x: 60, y: 100, name: "生科院", description: "叉叉!", distance: 0}, 
-        // {spotID: 8, x: 120, y: 120, name: "葉子", description: "就是個葉子", distance: 0},
+		// {spotID: 1, x: 50, y: 150, name: "人社院", description: "人社院是迷宮"},
+		// {spotID: 2, x: 70, y: 60, name: "台達館", description: "資工電神所在地"},
+		// {spotID: 3, x: 80, y: 90, name: "小吃部", description: "小吃部只有麥當勞"}, 
+		// {spotID: 4, x: 10, y: 10, name: "成功湖", description: "很大的一個湖"},
+		// {spotID: 5, x: 20, y: 130, name: "教育館", description: "上通識課的地方"},  
+        // {spotID: 6, x: 100, y: 100, name: "旺宏館", description: "這是旺宏館"}, 
+        // {spotID: 7, x: 60, y: 100, name: "生科院", description: "叉叉!"}, 
+        // {spotID: 8, x: 120, y: 120, name: "葉子", description: "就是個葉子"},
     ]);
+	const [distanceList, setDistanceList] = useState([])
 	const [pathName, setPathName] = useState('');
 	const [pathFinished, setPathFinished] = useState(false);
 	
+	const getUserLatLng = useCallback(() => {
+		// Dummy Fetch
+		navigator.geolocation.getCurrentPosition(()=>{}, ()=>{}, {});
+		const success = (position) => {
+			setUserLat(position.coords.latitude);
+			setUserLng(position.coords.longitude);
+		}
+		const fail = () => {};
+		navigator.geolocation.getCurrentPosition(
+			success, fail, {
+				enableHighAccuracy: true, 
+				timeout:10000
+			}
+		);
+	}, [])
+
 	const setFinished = useCallback(async () => {
 		console.log("Set Finished");
 		if(!localStorage.getItem('token')){
@@ -57,10 +79,9 @@ function PathPage() {
 		}))
 	}, [])
 
-	const getPath = useCallback(async () => {
+	const onLoad = useCallback( async () => {
 		// Part I: Get Path
-		console.log(pathID);
-        const response = await fetch('https://sdgs12.herokuapp.com/api/path', {
+		const response = await fetch('https://sdgs12.herokuapp.com/api/path', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
@@ -68,7 +89,7 @@ function PathPage() {
 			body: JSON.stringify({
 				pathID,
 			}),
-        })
+		})
 		const data = await response.json();
 		if(data.status === 'fail'){
 			setValid(false);
@@ -78,18 +99,15 @@ function PathPage() {
 		}
 
 		const pathData = data.pathData;
-		console.log("hi" + pathData);
 		setPathName(pathData.name);
 
 		const spotPath = data.spotPath;
-		console.log(spotPath);
 
 		// Part II: Get Spots
 		var spotIDList = [];
 		for(var i in spotPath){
-            spotIDList.push(spotPath[i].spotID);
-        };
-		console.log(spotIDList);
+			spotIDList.push(spotPath[i].spotID);
+		};
 
 		const response2 = await fetch('https://sdgs12.herokuapp.com/api/spotAll', {
 			method: 'POST',
@@ -108,43 +126,106 @@ function PathPage() {
 			return;
 		}
 		const spotData = data2.spotData;
-		console.log("SpotData: " + spotData);
 		setSpotList(spotData);
-		setUserX(70);
-		setUserY(70);
 		await setFinished();
-		setLoading(false);
-    }, [pathID, setFinished]);
 
-    useEffect(() => {
-        getPath();
-    }, [getPath]);
+		// Part III: SET PATH
+		const google = window.google;
+		const directionsService = new google.maps.DirectionsService();
+		var waypoints = [];
+		for (i=0; i<spotData.length; i++) {
+			if(i === 0 || i === spotData.length-1){
+				continue;
+			}
+			waypoints.push({
+				location: {lat: spotData[i].lat, lng: spotData[i].lng},
+				// location: spotData[i].name,
+				// stopover: false
+			})
+		}
+		var request = {
+			origin: {lat: spotData[0].lat, lng:spotData[0].lng},
+			destination: {lat: spotData[spotData.length-1].lat, lng:spotData[spotData.length-1].lng},
+			waypoints: waypoints,
+			travelMode: 'WALKING',
+			optimizeWaypoints: true
+		};
 
+		directionsService.route(request, function (result, status) {
+			var tempDuration = 0;
+			if (status === 'OK') {
+				for(var i in result.routes[0].legs) {
+					for(var j in result.routes[0].legs[i].steps) {
+						tempDuration += result.routes[0].legs[i].steps[j].duration.value;
+					}
+				}
+				// directionsDisplay.setDirections(result);
+				setPredictTime(tempDuration);
+			} else {
+				console.log(status);
+			}
+		});
+
+		let interval;
+		getUserLatLng();
+		interval = setInterval(getUserLatLng, 1000);
+		return () => clearInterval(interval);
+	}, [setFinished, getUserLatLng, pathID])
+	
+	useEffect(() => {
+		onLoad();
+	}, [onLoad])
+	
     useEffect(() => {
-        console.log("Set New Distance");
-        var newDistance;
-        setSpotList(spotList => spotList.map((spot) => {
-            newDistance = Math.round(Math.sqrt(Math.pow(userX-spot.lat, 2) + Math.pow(userY-spot.lng, 2)));
-            return {...spot, distance: newDistance}; 
-        }))
-    }, [userX, userY]);
+		let destinations = [];
+		for (var i in spotList) {
+			destinations.push({lat: spotList[i].lat, lng: spotList[i].lng});
+		}
+		if(navigator.geolocation){
+			if(userLat === 0) return;
+			function error() {
+				alert('無法取得你的位置');
+			}
+			async function success(position) {
+				const service = new window.google.maps.DistanceMatrixService();
+				await service.getDistanceMatrix({
+					origins: [{lat: userLat, lng: userLng}],
+					destinations: destinations,
+					travelMode: 'WALKING', // 交通方式：BICYCLING(自行車)、DRIVING(開車，預設)、TRANSIT(大眾運輸)、WALKING(走路)
+					unitSystem: window.google.maps.UnitSystem.METRIC, // 單位 METRIC(公里，預設)、IMPERIAL(哩)
+					avoidHighways: true, // 是否避開高速公路
+					avoidTolls: true // 是否避開收費路線
+				}, callback);  
+				function callback(response, status){
+					var tempList = [];
+					for(i in response.rows[0].elements) {
+						tempList.push(response.rows[0].elements[i].distance.value);
+					}
+					setDistanceList(tempList);
+				}
+				setLoading(false);
+			}
+			navigator.geolocation.getCurrentPosition(success, error);
+		} else {
+			alert('sorry')
+		}
+    }, [userLat, userLng, spotList]);
 
 	useEffect(() => {
 		if(spotList.length === 0) return;
 		console.log("Set Predict Time & PathFinished");
-		var newPredictTime = 0;
+		var finished = true;
 		for(var i in spotList){
-			if(!spotList[i].finished) newPredictTime += spotList[i].distance;
+			if(!spotList[i].finished) {
+				finished = false
+				break;
+			}
 		}
-		setPredictTime(newPredictTime);
-		if(newPredictTime === 0) {
-			setPathFinished(true);
-		}
+		if(finished) setPathFinished(true);
 	}, [spotList])
 
 	//disappearable shadow
     const [active, setActive] = useState(false);
-
     useEffect(() => {
         window.addEventListener('scroll', pop);
       
@@ -161,7 +242,7 @@ function PathPage() {
         // setNavColor(null);
     }
 	
-	if(loading) return <Load/>;
+	if(loading || !isLoaded) return <Load/>;
 	return (
 		<div className="Path">
 			{valid? (
@@ -190,16 +271,34 @@ function PathPage() {
 						</div>
 					</div>
 					<div className="pathSpots">
-						{spotList.map((spot) => {
+						{spotList.map((spot, id) => {
 							return ( 
 								<button className="card" key={spot.spotID} onClick={() => {window.location.href = '/spot/' + spot.spotID + '/' + pathID}}>
 									<h3>{spot.name}{spot.finished? "✅": ""}</h3>
-									<p>距離: {spot.distance}m</p>
+									<p>距離: {
+										!distanceList[id]? 
+										'?'
+										: 
+										<>{distanceList[id] >= 1000 ?
+											<>{Math.round(distanceList[id]/100)/10}公里</> 
+											:
+											<>{distanceList[id]}公尺</>}
+										</>}
+									</p>
 								</button>
 							)
 						})}
 					</div>
-					<h2 className="predictedTime">預計完成時間: {predictTime}分鐘</h2>
+					<h2 className="predictedTime">預計完成時間: {predictTime>=60? 
+						<>{Math.round(predictTime/60)}分鐘</>
+						: 
+						<>{predictTime===0? 
+							<>0分鐘</>
+							:
+							<>{"<"}1分鐘</>
+						}</>
+						}
+					</h2>
 					<button className='start-btn' onClick={() => {}}>開始</button>
 					{/* <button onClick={() => {window.location.href = '/path/' + pathID + '/map'}}>Map</button> */}
 				</>
