7a572582b4b23dd9c055f57a270a42d923aec5cb
David Wu <david.w@migo.io>
2022-09-07 17:23:01 +0800

    PathMap CSS

--- src/pages/path/pathmap.js
+++ src/pages/path/pathmap.js
@@ -279,7 +279,7 @@ function MyComponent() {
 									icon={{
 										path: "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z",
 										fillColor: "#6B9080",
-										fillOpacity: 0.7,
+										fillOpacity: 1,
 										scale: 2,
 										strokeColor: "white",
 										strokeWeight: 2,
@@ -293,7 +293,7 @@ function MyComponent() {
 								icon={{
 									path: "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z",
 									fillColor: "#365145",
-									fillOpacity: 0.8,
+									fillOpacity: 1,
 									scale: 2,
 									strokeColor: "white",
 									strokeWeight: 2,
