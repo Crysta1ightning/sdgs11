0240fa4bff486c021cc0b11338c876443425afa3
David Wu <0830david@gmail.com>
2022-09-12 16:37:55 +0800

    Profile let user change img

--- src/pages/auth/profile.js
+++ src/pages/auth/profile.js
@@ -3,7 +3,8 @@ import './profile.css';
 import jwtDecode from 'jwt-decode'
 import Navbar from '../global/navbar';
 import Load from '../global/load';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 
 function ProfilePage() {
 	const [loading, setLoading] = useState(true);
@@ -11,8 +12,11 @@ function ProfilePage() {
 	const [user, setUser] = useState({});
 	const [userImg, setUserImg] = useState();
 	const [backgroundImg, setBackgroundImg] = useState();
+	const [userImgList, setUserImgList] = useState([]);
+	const [backgroundImgList, setBackgroundImgList] = useState([]);
+	const [showUIL, setShowUIL] = useState(false);
+	const [showBIL, setShowBIL] = useState(false);
 
-	
 	async function getUser() {
 		console.log("Get User Data");
         const req = await fetch ('https://sdgs12.herokuapp.com/api/user', {
@@ -24,7 +28,41 @@ function ProfilePage() {
         const data = await req.json();
         if(data.status === 'ok') setUser(data.user);
         else alert(data.error);
-		setLoading(false);
+		
+		// GET USER IMG lIST
+		var tempUserImgList = [];
+		for(var i=1; i<=6; i++) {
+			try {
+				tempUserImgList.push(require('../../images/user/' + i + '.png'));
+			} catch (err) {
+				console.log(err);
+			}
+		}
+		setUserImgList(tempUserImgList);
+
+		// GET BACKGROUND IMG LIST
+		console.log("Fetch SpotData");
+        const response = await fetch('https://sdgs12.herokuapp.com/api/all', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data2 = await response.json();
+        if(data.status === 'fail'){
+            console.log("Failed to get spots");
+            return;
+        };
+        const spotData = data2.spotData;
+        var tempBackgroundImgList = [];
+        for(i in spotData){
+            try{
+                tempBackgroundImgList.push(require('../../images/spot/'+spotData[i].name+'.jpg'));
+            } catch (err) {
+                tempBackgroundImgList.push(require('../../images/spot/image-not-found.jpg'));
+            }
+        }
+		setBackgroundImgList(tempBackgroundImgList);
     }
 
 	useEffect(() => {
@@ -41,8 +79,6 @@ function ProfilePage() {
 			}else{
 			  setSignIn(true);
 			  getUser();
-			  setBackgroundImg(require('../../images/spot/旺宏館.jpg'));
-			  setUserImg(require('../../images/user/avatar.png'));
 			}
 		} else {
 			setBackgroundImg(require('../../images/spot/image-not-found.jpg'));
@@ -50,6 +86,19 @@ function ProfilePage() {
 			setLoading(false);
 		}
 	}, [])
+
+	useEffect(() => {
+		// userImg
+		if(userImgList.length === 0) return;
+		setUserImg(userImgList[0]);
+	}, [userImgList])
+
+	useEffect(() => {
+		// backgroundImg
+		if(backgroundImgList.length === 0) return;
+		setBackgroundImg(backgroundImgList[0]);
+		setLoading(false);
+	}, [backgroundImgList])
 
 	if(loading) return <Load/>;
 	return (
@@ -61,10 +110,16 @@ function ProfilePage() {
 					</div>
 					<Navbar/>
 					<div className="container">
-						<div className='bgimg-container'>
+						<div className='bgimg-container' onClick={() => {
+								setShowBIL(true);
+								setShowUIL(false);
+							}}>
 							<img src={backgroundImg} alt="圖片"></img>
 						</div>
-						<div className='userimg-container'>
+						<div className='userimg-container' onClick={() => {
+								setShowUIL(true);
+								setShowBIL(false);
+							}}>
 							<img src={userImg} alt="圖片"></img>
 						</div>
 						<h1 className='name'>{user.username}</h1>
@@ -81,6 +136,40 @@ function ProfilePage() {
 							{user.studentID &&<h4>學號: {user.studentID}</h4>}
 						</div>
 					</div>
+					{showBIL?
+						<div className='choose-container'>
+							<div className='cancel' onClick={() => {
+								setShowBIL(false);
+							}}><FontAwesomeIcon icon={solid('x')} /></div>
+							<h1>選擇要更換的背景</h1>
+							{backgroundImgList.map((bgimg, id) => {
+								return(
+									<img key={id} className='bgimg' src={bgimg} alt="背景" onClick={() => {
+										setBackgroundImg(backgroundImgList[id]);
+									}}></img>
+								)
+							})}
+						</div>
+						:
+						<div className='choose-container hide-container'></div>
+					}
+					{showUIL? 
+						<div className='choose-container'>
+							<div className='cancel' onClick={() => {
+								setShowUIL(false);
+							}}><FontAwesomeIcon icon={solid('x')} /></div>
+							<h1>選擇要更換的頭貼</h1>
+							{userImgList.map((userimg, id) => {
+								return (
+									<img key={id} className='userimg' src={userimg} alt="頭貼" onClick={() => {
+										setUserImg(userImgList[id]);
+									}}></img>
+								)
+							})}
+						</div>
+						:
+						<div className='choose-container hide-container'></div>
+					}
 				</div>
 				) : (
 				<div>
